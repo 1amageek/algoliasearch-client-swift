7d65b48bc4108ae3518e7912bfafc4ee6be5ae56
1amageek <nori@stamp.team>
2022-09-12 10:33:29 +0900

    Separate Modules

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let macOSVersion: SupportedPlatform.MacOSVersion = .v10_10
 
 #if os(Linux)
 let extraPackageDependencies: [Package.Dependency] = [
-  .package(url: "https://github.com/apple/swift-crypto.git", from: "1.1.2")
+    .package(url: "https://github.com/apple/swift-crypto.git", from: "1.1.2")
 ]
 #else
 let extraPackageDependencies: [Package.Dependency] = []
@@ -19,39 +19,86 @@ let extraPackageDependencies: [Package.Dependency] = []
 
 #if os(Linux)
 let extraTargetDependencies: [Target.Dependency] = [
-  .product(name: "Crypto", package: "swift-crypto")
+    .product(name: "Crypto", package: "swift-crypto")
 ]
 #else
 let extraTargetDependencies: [Target.Dependency] = []
 #endif
 
 let package = Package(
-  name: "AlgoliaSearchClient",
-  platforms: [
-    .iOS(.v9),
-    .macOS(macOSVersion),
-    .watchOS(.v2),
-    .tvOS(.v9)
-  ],
-  products: [
-    .library(
-      name: "AlgoliaSearchClient",
-      targets: ["AlgoliaSearchClient"])
-  ],
-  dependencies: [
-    .package(url: "https://github.com/apple/swift-log.git", from: "1.4.0")
-  ] + extraPackageDependencies,
-  targets: [
-    .target(
-      name: "AlgoliaSearchClient",
-      dependencies: [
-        .product(name: "Logging", package: "swift-log")
-      ] + extraTargetDependencies),
-    .testTarget(
-      name: "AlgoliaSearchClientTests",
-      dependencies: [
-        .target(name: "AlgoliaSearchClient"),
-        .product(name: "Logging", package: "swift-log")
-      ] + extraTargetDependencies)
-  ]
+    name: "AlgoliaSearchClient",
+    platforms: [
+        .iOS(.v9),
+        .macOS(macOSVersion),
+        .watchOS(.v2),
+        .tvOS(.v9)
+    ],
+    products: [
+        .library(name: "Core", targets: ["Core"]),
+        .library(name: "AccountClient", targets: ["AccountClient"]),
+        .library(name: "AnalyticsClient", targets: ["AnalyticsClient"]),
+        .library(name: "InsightsClient", targets: ["InsightsClient"]),
+        .library(name: "RecommendClient", targets: ["RecommendClient"]),
+        .library(name: "PersonalizationClient", targets: ["PersonalizationClient"]),
+        .library(name: "PlacesClient", targets: ["PlacesClient"]),
+        .library(name: "SearchClient", targets: ["SearchClient"]),
+        .library(name: "AlgoliaSearchClient", targets: ["AlgoliaSearchClient"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-log.git", from: "1.4.0")
+    ] + extraPackageDependencies,
+    targets: [
+        .target(
+            name: "Core",
+            dependencies: [
+                .product(name: "Logging", package: "swift-log")
+            ] + extraTargetDependencies),
+        .target(
+            name: "AccountClient",
+            dependencies: ["Core"]
+        ),
+        .target(
+            name: "AnalyticsClient",
+            dependencies: ["Core"]
+        ),
+        .target(
+            name: "InsightsClient",
+            dependencies: ["Core"]
+        ),
+        .target(
+            name: "RecommendClient",
+            dependencies: ["Core"]
+        ),
+        .target(
+            name: "PersonalizationClient",
+            dependencies: ["Core"]
+        ),
+        .target(
+            name: "PlacesClient",
+            dependencies: ["Core"]
+        ),
+        .target(
+            name: "SearchClient",
+            dependencies: ["Core"]
+        ),
+        .target(
+            name: "AlgoliaSearchClient",
+            dependencies: [
+                "AccountClient",
+                "AnalyticsClient",
+                "InsightsClient",
+                "RecommendClient",
+                "PersonalizationClient",
+                "PlacesClient",
+                "SearchClient"
+            ]
+        ),
+        
+        //        .testTarget(
+        //            name: "AlgoliaSearchClientTests",
+        //            dependencies: [
+        //                .target(name: "AlgoliaSearchClient"),
+        //                .product(name: "Logging", package: "swift-log")
+        //            ] + extraTargetDependencies)
+    ]
 )
---
+++ Sources/AlgoliaSearchClient/AlgoliaSearchClient.swift
@@ -0,0 +1,15 @@
+//
+//  AlgoliaClient.swift
+//  
+//
+//  Created by Norikazu Muramoto on 2022/09/12.
+//
+
+import Foundation
+@_exported import AccountClient
+@_exported import AnalyticsClient
+@_exported import InsightsClient
+@_exported import PersonalizationClient
+@_exported import PlacesClient
+@_exported import RecommendClient
+@_exported import SearchClient
